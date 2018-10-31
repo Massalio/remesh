c75a24dfad6082c535466b4d2b318d7c6fd9926f
massalio <lioneldagnino@gmail.com>
2018-10-31 14:35:16 -0300

    Servidor del Massa 2.9

--- config/mongodb.py
+++ config/mongodb.py
@@ -1,7 +1,7 @@
 import os
 from pymongo import MongoClient
 
-mongo_uri = "mongodb://rocky-headland-53719.herokuapp.com/api/v1"
+mongo_uri = "mongodb://rocky-headland-53719.herokuapp.com/api/v1:8000"
 if "MONGO_URL" in os.environ:
     mongo_uri = os.environ['MONGO_URL'] + ":" + os.environ['MONGO_PORT']
 
